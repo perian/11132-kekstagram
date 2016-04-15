792ed8029fba3a5f155ecd34b16883d5e95a28e1
perian <dmperian@gmail.com>
2016-04-15 04:48:02 +0300

    Шаблонизация
    Почему надо задавать ссылку для изображения уже после загрузки?

--- src/pictures.js
+++ src/pictures.js
@@ -5,32 +5,64 @@ if (!document.querySelector('.filters.hidden')) {
   hiddenBlockOfFilters.classList.add('hidden');
 }
 
+var pictureUploadExpectant;
 var picturesContainer = document.querySelector('.pictures');
-var pictureTemplateElement = document.querySelector('template');
-var elementToClone;
+var pictureTemplateElement = document.getElementById('picture-template');
 
+/*
+* Задаем ссылку для копирования элементов,
+* в зависимости от того поддерживает ли сайт тэг teamplate
+*/
+var elementToClone;
 if ('content' in pictureTemplateElement) {
-  elementToClone = pictureTemplateElement.content.children[0].cloneNode(true);
+  elementToClone = pictureTemplateElement.content.children[0];
 } else {
-  elementToClone = pictureTemplateElement.children[0].cloneNode(true);
+  elementToClone = pictureTemplateElement.children[0];
 }
 
+/*
+*
+*/
 var getPictureTemplate = function(data) {
-  elementToClone.querySelector('.picture-comments').textContent = data.comments;
-  elementToClone.querySelector('.picture-likes').textContent = data.likes;
-  picturesContainer.appendChild(elementToClone);
-  var image = new Image();
-  image.onload = function(evt) {
-    elementToClone.querySelector('img').setAttribute('src', evt.target.src);
-    image.width = 182;
-    image.height = 182;
-  };
-  //
-  // image.onerror = function() {
-  //   picturesContainer.classList.add('picture-load-failure');
-  // };
+  var PICTURE_UPLOAD_TIMEOUT = 10000;
+  var picturesContainerElements = elementToClone.cloneNode(true);
+  picturesContainerElements.querySelector('.picture-comments').textContent = data.comments;
+  picturesContainerElements.querySelector('.picture-likes').textContent = data.likes;
+  picturesContainer.appendChild(picturesContainerElements);
 
-  image.src = data.preview;
+  /*
+  *
+  */
+  var image = new Image();
+  image = elementToClone.querySelector('img');
+
+  image.onload = function() {
+    clearTimeout(pictureUploadExpectant);
+  };
+
+  image.width = 182;
+  image.height = 182;
+  image.src = data.url;
+
+  image.onerror = function() {
+    elementToClone.classList.add('picture-load-failure');
+  };
+
+  /*
+  * Если за 10 секкунд картинка не загрузилась, прерывает загрузку.
+  */
+  pictureUploadExpectant = setTimeout(function() {
+    image.src = '';
+    elementToClone.classList.add('picture-load-failure');
+  }, PICTURE_UPLOAD_TIMEOUT);
+
+
+  /*
+  * Отображает блок с фильтрами. После загрузки содержимого сайта.
+  */
+  if (document.querySelector('.filters.hidden')) {
+    hiddenBlockOfFilters.classList.remove('hidden');
+  }
 };
 
 window.pictures.forEach(function(picture) {
