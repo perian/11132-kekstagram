c09ded06c353ac1812574520ea75157f11dfb00e
perian <dmperian@gmail.com>
2016-04-20 16:22:24 +0300

    Передача и обработка данных

--- src/pictures.js
+++ src/pictures.js
@@ -8,6 +8,12 @@ if (!document.querySelector('.filters.hidden')) {
 var picturesContainer = document.querySelector('.pictures');
 var pictureTemplateElement = document.getElementById('picture-template');
 
+var FILTER = {
+  'POPULAR': 'filter-popular',
+  'NEW': 'filter-new',
+  'DISCUSSED': 'filter-discussed'
+};
+
 /*
 * Задаем ссылку для копирования элементов,
 * в зависимости от того поддерживает ли сайт тэг teamplate
@@ -20,20 +26,16 @@ if ('content' in pictureTemplateElement) {
 }
 
 /*
-*
+* Отрисовывает загруженные данные по шаблону
 */
 var getPictureTemplate = function(data) {
   var pictureUploadExpectant;
   var PICTURE_UPLOAD_TIMEOUT = 10000;
   var picturesContainerElements = elementToClone.cloneNode(true);
+  var image = picturesContainerElements.querySelector('img');
   picturesContainerElements.querySelector('.picture-comments').textContent = data.comments;
   picturesContainerElements.querySelector('.picture-likes').textContent = data.likes;
   picturesContainer.appendChild(picturesContainerElements);
-
-  /*
-  *
-  */
-  var image = picturesContainerElements.querySelector('img');
 
   image.onload = function() {
     clearTimeout(pictureUploadExpectant);
@@ -63,6 +65,102 @@ var getPictureTemplate = function(data) {
   }
 };
 
-window.pictures.forEach(function(picture) {
-  getPictureTemplate(picture, pictureTemplateElement);
+/*
+* Используем xmlHttpRequest для загрузки массива обьектов с сервера
+*/
+var getPictures = function(callback) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', '//o0.github.io/assets/json/pictures.json');
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== 4 ) {
+      picturesContainer.classList.add('pictures-loading');
+    }
+  };
+
+  xhr.onload = function(evt) {
+    var loadedData = JSON.parse(evt.target.response);
+    callback(loadedData);
+    if (xhr.readyState === 4 ) {
+      picturesContainer.classList.remove('pictures-loading');
+    }
+  };
+
+  xhr.onerror = function() {
+    if (xhr.status !== 200 ) {
+      picturesContainer.classList.add('pictures-failure');
+    }
+  };
+
+  xhr.timeout = 10000;
+  xhr.ontimeout = function() {
+    picturesContainer.classList.add('pictures-failure');
+  };
+
+  xhr.send();
+};
+
+/**
+* Отрисовывает каждый элемент загруженных данных по шаблону.
+* @param {Array.<Object>} pictures
+*/
+var renderPictures = function(pictures) {
+  pictureTemplateElement.innerHTML = '';
+
+  pictures.forEach(function(picture) {
+    getPictureTemplate(picture, pictureTemplateElement);
+  });
+};
+
+/**
+* Сортирует данные
+* @param {string} filter
+* @param {Array.<Object>} pictures
+*/
+var getFilteredPictures = function(pictures, filter) {
+  debugger;
+  var picturesToFilter = pictures.slice(0);
+
+  switch (filter) {
+    case FILTER.NEW:
+      picturesToFilter.sort(function(a, b) {
+        if (Date.parse(a.date) > Date.parse(b.date)) {
+          return 1;
+        }
+        if (Date.parse(a.date) < Date.parse(b.date)) {
+          return -1;
+        }
+      });
+      break;
+  }
+
+  return picturesToFilter;
+};
+
+/**
+* Перерисовка загруженных данных в соответствии с выбранной кнопкой фильтра
+* @param {string} filter
+*/
+var setFilterOnButton = function(pictures, filter) {
+  var filteredPictures = getFilteredPictures(pictures, filter);
+  renderPictures(filteredPictures);
+};
+
+/*
+* Включение кнопок cортировки загруженных данных
+*/
+var tunrOnFilterButtons = function(pictures) {
+  var filterButtons = hiddenBlockOfFilters.querySelectorAll('[name="filter"]');
+  for (var i = 0; i < filterButtons.length; i++) {
+    filterButtons[i].onclick = function() {
+      setFilterOnButton(pictures, this.id);
+    };
+  }
+};
+
+getPictures(function(loadedData) {
+  var pictures = loadedData;
+  renderPictures(pictures);
+  tunrOnFilterButtons(pictures);
 });
