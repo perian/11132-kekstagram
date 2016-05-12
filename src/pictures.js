4356ebf6642e7db9be6b417ac89bb6248fd4321d
perian <dmperian@gmail.com>
2016-05-12 06:57:53 +0300

    Первый класс...

--- src/pictures.js
+++ src/pictures.js
@@ -1,22 +1,20 @@
 'use strict';
 
 (function() {
-  var gallery = require('./gallery');
-
   /**
   * Обьект который отрисовывет каждый элемент загруженных данных
   * @type {Photo}
   */
   var Photo = require('./render-picture');
 
+  var gallery = require('./gallery');
+
   var blockOfFilters = document.querySelector('.filters');
+  var picturesContainer = document.querySelector('.pictures');
+  var pictureTemplateElement = document.getElementById('picture-template');
   if (!document.querySelector('.filters.hidden')) {
     blockOfFilters.classList.add('hidden');
   }
-
-  var picturesContainer = document.querySelector('.pictures');
-  var pictureTemplateElement = document.getElementById('picture-template');
-  var image;
 
   /** @type {Array.<Object>} */
   var filteredPictures = [];
@@ -33,68 +31,13 @@
   /** constant {number} */
   var PAGE_SIZE = 12;
 
-  /** @type {number} */
-  var imageHeight = 0;
+  // /** @type {number} */
+  // var imageHeight = 0;
 
   /** @type {number} */
   var pageNumber = 0;
 
-  /**
-  * Задаем ссылку для копирования элементов,
-  * в зависимости от того поддерживает ли сайт тэг teamplate.
-  */
-  var elementToClone;
-  if ('content' in pictureTemplateElement) {
-    elementToClone = pictureTemplateElement.content.children[0];
-  } else {
-    elementToClone = pictureTemplateElement.children[0];
-  }
-
-  /**
-  * Отрисовывает загруженные данные по шаблону.
-  * @param {Object} data
-  */
-  var getPictureTemplate = function(data) {
-    var pictureUploadExpectant;
-    var PICTURE_UPLOAD_TIMEOUT = 10000;
-    var picturesContainerElements = elementToClone.cloneNode(true);
-    image = picturesContainerElements.querySelector('img');
-    picturesContainerElements.querySelector('.picture-comments').textContent = data.comments;
-    picturesContainerElements.querySelector('.picture-likes').textContent = data.likes;
-    picturesContainer.appendChild(picturesContainerElements);
-
-    image.onload = function() {
-      clearTimeout(pictureUploadExpectant);
-    };
-
-    image.width = 182;
-    image.height = 182;
-    image.src = data.url;
-
-    imageHeight = image.height;
-
-    image.onerror = function() {
-      picturesContainerElements.classList.add('picture-load-failure');
-    };
-
-    /**
-    * Если за 10 секунд картинка не загрузилась, прерывает загрузку.
-    */
-    pictureUploadExpectant = setTimeout(function() {
-      image.src = '';
-      picturesContainerElements.classList.add('picture-load-failure');
-    }, PICTURE_UPLOAD_TIMEOUT);
-
-    /**
-    * Отображает блок с фильтрами. После загрузки содержимого сайта.
-    */
-    if (document.querySelector('.filters.hidden')) {
-      blockOfFilters.classList.remove('hidden');
-    }
-    gallery.setGalleryPictures(filteredPictures);
-
-    return picturesContainerElements;
-  };
+  gallery.setGalleryPictures(filteredPictures);
 
   /**
   * Используем xmlHttpRequest для загрузки массива обьектов с сервера.
@@ -205,9 +148,9 @@
   */
   var isBottomReached = function() {
     var picturesContainerCoordinates = picturesContainer.getBoundingClientRect();
-    var GAP = 100;
+    var GAP = 300;
 
-    return picturesContainerCoordinates.bottom - imageHeight - window.innerHeight - GAP <= 0;
+    return picturesContainerCoordinates.bottom - window.innerHeight - GAP <= 0;
   };
 
   /**
@@ -264,6 +207,4 @@
     setFilterOnButton(pictures);
     setScrollEnabled();
   });
-
-  module.exports = getPictureTemplate;
 })();
--- src/render-picture.js
+++ src/render-picture.js
@@ -1,27 +1,26 @@
 'use strict';
 
-var getPictureTemplate = require('./pictures');
 var gallery = require('./gallery');
+var getPictureTemplate = require('get-picture-template');
 
 /**
 * @param {Object} data
 * @constructor
 */
-var Photo = function(picture, pictureTemplateElement) {
-  this.data = picture;
+var Photo = function(data, pictureTemplateElement) {
+  this.data = data;
   this.element = getPictureTemplate(this.data, pictureTemplateElement);
   this.onPhotoClick = function() {
     gallery.getIndexOfPicture();
   };
 
   this.element.addEventListener('click', this.onPhotoClick);
-  pictureTemplateElement.appendChild(this.element);
 
   this.remove = function() {
     this.element.removeEventListener('click', this.onPhotoClick);
     this.element.parentNode.removeChild(this.element);
-    pictureTemplateElement.appendChild('');
   };
+  pictureTemplateElement.appendChild(this.element);
 };
 
 module.exports = Photo;
